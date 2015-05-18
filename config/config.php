ff2f362c1fd83dfad5f51a10a2429d9300f8cd82
ipiinto <pkscm_pin@hotmail.com>
2015-05-18 20:14:20 +0700

    config
    config

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+	$host="localhost";
+	$hostuser="root";
+	$hostpass="1234";
+	$database="new_training";
+	$list_page = 25;
+	$ribon="ระบบจัดการสถาบันกวดวิชา | Academic Management System";
+?>
